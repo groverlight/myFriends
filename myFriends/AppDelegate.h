855476ea7db7ea1654942c5ac29bdc571984a24c
Grover Light <grover.light@gmail.com>
2015-03-24 14:55:05 -0700

    first commit

---
+++ myFriends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myFriends
+//
+//  Created by Grover Light on 3/23/15.
+//  Copyright (c) 2015 learning, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myFriends/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  myFriends
+//
+//  Created by Grover Light on 3/23/15.
+//  Copyright (c) 2015 learning, inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property NSArray *friendsArray;
+@property NSArray *adjectivesArray;
+
+@property int randomInt;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+    self.friendsArray = [NSArray arrayWithObjects:@"Emily", @"Phillip", @"Robin", @"Bill", @"Kristin", nil];
+    self.adjectivesArray = [NSArray arrayWithObjects:@"nosey", @"aloof", @"cunty", @"gay", @"carefree", nil];
+
+
+    for (NSString *friend in self.friendsArray)
+
+    {
+
+        int randomInt = arc4random_uniform(4);
+        NSString *adjective = [self.adjectivesArray objectAtIndex:randomInt];
+        NSLog(@"%@ is %@", friend, adjective);
+
+    }
+
+
+}
+
+
+@end
